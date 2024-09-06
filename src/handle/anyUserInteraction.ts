991e9141045cc5e9111ad8f81de00aa730050677
jniac <joseph.merdrignac@gmail.com>
2024-09-06 18:56:30 +0200

    handleAnyUserInteraction

---
+++ src/handle/anyUserInteraction.ts
@@ -0,0 +1,31 @@
+import { DestroyableObject } from 'some-utils-ts/types'
+
+export function handleAnyUserInteraction(callback: () => void): DestroyableObject
+export function handleAnyUserInteraction(element: HTMLElement | Window, callback: () => void): DestroyableObject
+export function handleAnyUserInteraction(...args: any[]): DestroyableObject {
+  const [element, callback] = args.length === 1 ? [window, args[0]] : args
+  const onInteraction = () => {
+    callback()
+  }
+  element.addEventListener('mousemove', onInteraction)
+  element.addEventListener('mousedown', onInteraction)
+  element.addEventListener('mouseup', onInteraction)
+  element.addEventListener('touchstart', onInteraction)
+  element.addEventListener('touchmove', onInteraction)
+  element.addEventListener('wheel', onInteraction)
+  element.addEventListener('keydown', onInteraction)
+  element.addEventListener('keyup', onInteraction)
+  window.addEventListener('resize', onInteraction)
+  const destroy = () => {
+    element.removeEventListener('mousemove', onInteraction)
+    element.removeEventListener('mousedown', onInteraction)
+    element.removeEventListener('mouseup', onInteraction)
+    element.removeEventListener('touchstart', onInteraction)
+    element.removeEventListener('touchmove', onInteraction)
+    element.removeEventListener('wheel', onInteraction)
+    element.removeEventListener('keydown', onInteraction)
+    element.removeEventListener('keyup', onInteraction)
+    window.removeEventListener('resize', onInteraction)
+  }
+  return { destroy }
+}
