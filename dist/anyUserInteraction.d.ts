53b1cfdc1237a8df84665fd46a549e507f36cae7
jniac <joseph.merdrignac@gmail.com>
2024-09-06 19:11:25 +0200

    my first build

---
+++ dist/anyUserInteraction.d.ts
@@ -0,0 +1,3 @@
+import { DestroyableObject } from 'some-utils-ts/types';
+export declare function handleAnyUserInteraction(callback: () => void): DestroyableObject;
+export declare function handleAnyUserInteraction(element: HTMLElement | Window, callback: () => void): DestroyableObject;
