522bd50ab13baa8a5830ded576d900aa472fe80d
jniac <joseph.merdrignac@gmail.com>
2024-09-09 11:48:20 +0200

    handle/pointer: fixing touch/mouse double fire

--- src/handle/pointer/basic.ts
+++ src/handle/pointer/basic.ts
@@ -32,7 +32,8 @@ function hasBasicPointerCallback(params: Record<string, any>): boolean {
 
 /**
  * MEMO:
- *  - "pointer" events are fire BEFORE any "mouse" or "touch" events.
+ *  - "pointer" events are fire BEFORE any "mouse" or "touch" events, order is:
+ *    - pointer -> touch -> mouse
  * 
  * For multiple reasons related to the "mouse" & "touch" reunification, 
  * "mouse" & "touch" are handled separately (that is to say: not through the 
@@ -45,8 +46,30 @@ function hasBasicPointerCallback(params: Record<string, any>): boolean {
  * - "onChange" has to be called before "onDown" (ex: for updating a raycaster), 
  *   so this requires to use the "mousedown" & "touchstart" and not "pointer" 
  *   since ("pointer" are fired first, "touchstart" will necesseray be late).
+ * 
+ * NOTE:
+ * 
+ * A special effort is made to handle to avoid double "down/up" calls when "touch"
+ * events are fired (when "touch" events are fired, "mouse" events are also fired).
+ * Since in debug mode, it is possible to switch between "touch" & "mouse" events
+ * an heuristic is used that combine "touch started" state and "touch timestamp".
+ * This allows to go back and forth between "touch" & "mouse" events without 
+ * reloading the page.
  */
 function handleBasicPointer(element: PointerTarget, params: Params): () => void {
+  const state = {
+    touchStarted: false,
+    touchTimestamp: -1,
+  }
+  /**
+   * We consider that the user is using a touch device if:
+   * - any touch event has been fired in the last second
+   * - or if a touch "down" is currently happening (that could be a long press, with no move).
+   */
+  function isTouch() {
+    return state.touchStarted || state.touchTimestamp > Date.now() - 1000
+  }
+
   const info: BasicPointerInfo = {
     pressed: false,
     entered: false,
@@ -88,15 +111,21 @@ function handleBasicPointer(element: PointerTarget, params: Params): () => void 
 
   // MOUSE:
   const onMouseDown = (event: MouseEvent) => {
-    updateDown(event, event.clientX, event.clientY)
+    if (isTouch() === false) {
+      updateDown(event, event.clientX, event.clientY)
+    }
   }
 
   const onMouseUp = (event: MouseEvent) => {
-    updateUp(event, event.clientX, event.clientY)
+    if (isTouch() === false) {
+      updateUp(event, event.clientX, event.clientY)
+    }
   }
 
   const onMouseMove = (event: MouseEvent) => {
-    update(event, event.clientX, event.clientY)
+    if (isTouch() === false) {
+      update(event, event.clientX, event.clientY)
+    }
   }
 
   const onMouseOver = () => {
@@ -134,18 +163,23 @@ function handleBasicPointer(element: PointerTarget, params: Params): () => void 
   const onTouchStart = (event: TouchEvent) => {
     // Because when the touch start the position is not the same as the previous touch.
     if (event.touches.length === 1) { // ignore multi-touch
+      state.touchStarted = true
+      state.touchTimestamp = Date.now()
       updateDown(event, event.touches[0].clientX, event.touches[0].clientY)
     }
   }
 
   const onTouchEnd = (event: TouchEvent) => {
     if (event.touches.length === 0) { // ignore multi-touch
+      state.touchStarted = false
+      state.touchTimestamp = Date.now()
       updateUp(event, event.changedTouches[0].clientX, event.changedTouches[0].clientY)
     }
   }
 
   const onTouchMove = (event: TouchEvent) => {
     if (event.touches.length === 1) { // ignore multi-touch
+      state.touchTimestamp = Date.now()
       update(event, event.touches[0].clientX, event.touches[0].clientY)
     }
   }
