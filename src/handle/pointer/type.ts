eb25749acb64241c1c13bb80fe5d100cac3c85de
jniac <joseph.merdrignac@gmail.com>
2024-10-01 12:34:57 +0200

    PointerButon codes: iso with mdn specs

--- src/handle/pointer/type.ts
+++ src/handle/pointer/type.ts
@@ -1,14 +1,14 @@
 export enum PointerButton {
   // From the "mouse" specs:
   // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
-  Main = 1 << 0,
-  Aux = 1 << 1,
-  Second = 1 << 2,
-  Fourth = 1 << 3,
-  Fifth = 1 << 4,
+  Main = 0,
+  Aux = 1,
+  Second = 2,
+  Fourth = 3,
+  Fifth = 4,
 
   // Custom
-  DoubleTouch = 1 << 5,
+  DoubleTouch = 5,
 }
 
 export type PointerTarget =
